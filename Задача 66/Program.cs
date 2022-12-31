8954a9b5daa5fe3b4fe60c6518726ccbb914cbbf
SergeyKaraman <karamans7997@gmail.com>
2022-12-31 18:39:23 +0300

    Урок 9. Рекурсия

---
+++ Задача 66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int m = InputNumber("Введите значение для M: ");
+int n = InputNumber("Введите значение для N: ");
+
+int InputNumber(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+int SumNaturalElements(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + SumNaturalElements(m, n - 1);
+}
+Console.WriteLine($"Сумма элементов от {m} до {n} равна: {SumNaturalElements(m, n)}");
---
+++ Задача 68/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int m = InputNumber("Введите значение для M: ");
+int n = InputNumber("Введите значение для N: ");
+int FunctionAkkerman = Akk(m, n);
+
+int Akk(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Akk(m - 1, 1);
+    else return Akk(m - 1, Akk(m, n - 1));
+}
+
+int InputNumber(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+Console.Write($"Функция Аккермана = {FunctionAkkerman} ");
